b1492f7583c12c804e3e575feb0661656f35a4a1
Christian Chapman <cdchap@protonmail.com>
2020-10-22 17:22:40 -0500

    changed some wording

--- resources/views/livewire/bowl/bowl-list.blade.php
+++ resources/views/livewire/bowl/bowl-list.blade.php
@@ -1,5 +1,5 @@
 <div class=" container mx-auto px-4 sm:px-6 lg:px-8 ">
-    <h2 class="my-8 font-black font-sans tracking-wide uppercase text-4xl border-b-4 border-red-600">2020 Bowl Results</h2>
+    <h2 class="my-8 font-black font-sans tracking-wide uppercase text-4xl border-b-4 border-red-600">2020 Bowl Schedule</h2>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         @foreach($bowls as $bowl)
             <div class="shadow-black border-2 border-red-600 px-4 py-4 ">
@@ -10,7 +10,7 @@
                     <span class=" text-xs underline">{{ $bowl->date }}</span> 
                 </div>
                 <div class="flex flex-col justify-center mt-4 mb-4">
-                    <div class="">
+                    <div class="font-sans">
                         <span>{{ $bowl->visitor->name }}</span>
                         <span>vs.</span>
                         <span>{{ $bowl->home->name }}</span>
