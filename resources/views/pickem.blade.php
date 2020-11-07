4a12cdc9931de86c29bfb0cb02ac7d0e39945199
Christian Chapman <cdchap@protonmail.com>
2020-11-06 18:29:17 -0600

    syled pick page when they have already picked

--- resources/views/pickem.blade.php
+++ resources/views/pickem.blade.php
@@ -2,11 +2,18 @@
 
 @section('content')
 
-@can('make picks')
-   <livewire:bowl.pick-form />
-@else
-    <a href="{{ route('home') }}">go home</a>
-@endcan
+    @can('make picks')
+        <livewire:bowl.pick-form />
+    @else
+        <div class="h-screen flex flex-col justify-center items-center bg-gradient-to-br from-purple-400 via-pink-500 to-red-500">
+            <div class="w-1/3 border-2 border-black shadow-black px-4 py-6 bg-white">
+                <h3 class="text-2xl">🛑 Hey! ✋</h3>
+                <p class="text-cool-gray-900">You have already made your picks <a href="#"></a><span class="border-b-2 border-pink-500">&#64;{{ auth()->user()->username }}</span>! 
+                    Head back to the <a href="{{ route('home') }}" class="text-pink-500 underline hover:text-purple-500">home page</a>
+                </p>
+            </div>
+        </div>
+        
+    @endcan
 
-    
 @endsection
