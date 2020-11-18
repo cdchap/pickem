581f5951b120343799ade9552f60dc6d3d32a167
Christian Chapman <cdchap@protonmail.com>
2020-11-18 06:32:41 -0600

    tweeked leader board

--- resources/views/livewire/score/total-score.blade.php
+++ resources/views/livewire/score/total-score.blade.php
@@ -18,25 +18,13 @@
                         <div class="flex items-center px-4 py-4 sm:px-6">
                             <div class="min-w-0 flex-1 flex items-center">
                                 <div class="flex-shrink-0">
-                                    <img class="h-12 w-12 rounded-full"
-                                        src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                        alt="">
+                                    <span>{{ $key +1 }}.</span>
                                 </div>
                                 <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
-                                    <div>
+                                    <div class="flex justify-start items-center">
                                         <div class="text-sm leading-5 font-medium text-indigo-600 truncate">
                                             &#64;{{ $score['username'] }}
                                         </div>
-                                        {{-- <div class="mt-2 flex items-center text-sm leading-5 text-gray-500">
-                                            <!-- Heroicon name: mail -->
-                                            <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                                <path
-                                                    d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                                                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                                            </svg>
-                                            <span class="truncate">kristen.ramos@example.com</span>
-                                        </div> --}}
                                     </div>
                                     <div class="block">
                                         <div>
