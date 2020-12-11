a4c70cb27f57a3b78906b9ca0e79984268da6837
Christian Chapman <cdchap@protonmail.com>
2020-12-11 09:13:04 -0600

    added close filter tab

--- resources/views/livewire/user/user-index.blade.php
+++ resources/views/livewire/user/user-index.blade.php
@@ -35,6 +35,20 @@
         @if ($showFilters)
             <div class="bg-gradient-four py-8 px-4 rounded-lg shadow-inner">
                 <div class="grid grid-cols-2 gap-6">
+                    <div class="col-span-2 flex justify-between">
+                        <span class="inline-flex rounded-md shadow-sm">
+                            <button 
+                                wire:click="clearFilters"
+                                type="button"
+                                class="inline-flex items-center px-2.5 py-1.5 border border-gray-300 text-xs leading-4 font-medium rounded text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150">
+                                Clear filters
+                            </button>
+                        </span>
+                        <svg wire:click="toggleShowFilters"
+                            class="h-6 w-6 px-1 py-1 rounded-full hover:bg-gray-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </div>
                     <div class="">
                         <label for="location" class="block text-sm leading-5 font-medium text-gray-700">Permsissions</label>
                         <select id="location" 
@@ -58,16 +72,6 @@
                             @endforeach
                         </select>
                        </div>
-                    </div>
-                    <div class="bg-green-300">
-                        <span class="inline-flex rounded-md shadow-sm">
-                            <button 
-                                wire:click="clearFilters"
-                                type="button"
-                                class="inline-flex items-center px-2.5 py-1.5 border border-gray-300 text-xs leading-4 font-medium rounded text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150">
-                                Clear filters
-                            </button>
-                        </span>
                     </div>
                 </div>
             </div>
