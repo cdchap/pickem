1b092426e91c70984de8600a1ffac0f2a5f49ff8
Christian Chapman <cdchap@protonmail.com>
2020-12-10 18:09:00 -0600

    origin top

--- resources/views/livewire/accordian-picks-table.blade.php
+++ resources/views/livewire/accordian-picks-table.blade.php
@@ -1,68 +1,67 @@
-
-    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 min-w-max-content">
-        <div class="py-2 align-middle inline-block sm:px-6 lg:px-8">
-            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <table class="divide-y divide-gray-200">
-                    <thead>
-                        <tr>
-                            <th
-                                class="hidden md:inline-block px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                Matchup
-                            </th>
-                            <th
-                                class="hidden md:inline-block px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                Bowl Winner
-                            </th>
-                            <th
-                                class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                &#64;{{ $user->username }} Pick
-                            </th>
-                            <th
-                                class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                Confidence
-                            </th>
-                            
+<div class="origin-top -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 min-w-max-content">
+    <div class="py-2 align-middle inline-block sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table class="divide-y divide-gray-200">
+                <thead>
+                    <tr>
+                        <th
+                            class="hidden md:inline-block px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                            Matchup
+                        </th>
+                        <th
+                            class="hidden md:inline-block px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                            Bowl Winner
+                        </th>
+                        <th
+                            class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                            &#64;{{ $user->username }} Pick
+                        </th>
+                        <th
+                            class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                            Confidence
+                        </th>
+                        
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach ($picks as $pick)
+                        <tr class="{{ $pick->team_id == ($pick->bowl->winner ? $pick->bowl->winner->api_id : null) ? 'bg-green-100' : ''}}">
+                            <td
+                                class="hidden md:inline-block px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
+                                <div class="flex flex-col items-start">
+                                    <span class="inline-flex space-x-1 items-center" style="color: {{ $pick->bowl->visitor->color }}">
+                                        <img src="{{ $pick->bowl->visitor->logo1 }}" class="h-4 w-4" alt="" srcset="">
+                                        <span>{{ $pick->bowl->visitor->abbreviation }}</span>
+                                    </span> 
+                                    
+                                    <span class="inline-flex space-x-1 items-center" style="color: {{ $pick->bowl->home->color }}">
+                                        <img src="{{ $pick->bowl->home->logo1 }}" class="h-4 w-4" alt="" srcset="">
+                                        <span>{{ $pick->bowl->home->abbreviation }}</span>
+                                    </span>
+                                </div>
+                            </td>
+                            <td
+                                class="hidden md:inline-block px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-400">
+                                {{ $pick->bowl->winner->name ?? 'N/A' }}
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500 {{ $pick->team_id == ($pick->bowl->winner ? $pick->bowl->winner->id : null) ? 'text-green-500' : '' }}">
+                                {{ $pick->team->name }} 
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500 text-center {{ $pick->team_id == ($pick->bowl->winner ? $pick->bowl->winner->id : null) ? 'text-green-500' : '' }}">
+                                {{ $pick->confidence }}
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody class="bg-white divide-y divide-gray-200">
-                        @foreach ($picks as $pick)
-                            <tr class="{{ $pick->team_id == ($pick->bowl->winner ? $pick->bowl->winner->api_id : null) ? 'bg-green-100' : ''}}">
-                                <td
-                                    class="hidden md:inline-block px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
-                                    <div class="flex flex-col items-start">
-                                        <span class="inline-flex space-x-1 items-center" style="color: {{ $pick->bowl->visitor->color }}">
-                                            <img src="{{ $pick->bowl->visitor->logo1 }}" class="h-4 w-4" alt="" srcset="">
-                                            <span>{{ $pick->bowl->visitor->abbreviation }}</span>
-                                        </span> 
-                                        
-                                        <span class="inline-flex space-x-1 items-center" style="color: {{ $pick->bowl->home->color }}">
-                                            <img src="{{ $pick->bowl->home->logo1 }}" class="h-4 w-4" alt="" srcset="">
-                                            <span>{{ $pick->bowl->home->abbreviation }}</span>
-                                        </span>
-                                    </div>
-                                </td>
-                                <td
-                                    class="hidden md:inline-block px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-400">
-                                    {{ $pick->bowl->winner->name ?? 'N/A' }}
-                                </td>
-                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500 {{ $pick->team_id == ($pick->bowl->winner ? $pick->bowl->winner->id : null) ? 'text-green-500' : '' }}">
-                                    {{ $pick->team->name }} 
-                                </td>
-                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500 text-center {{ $pick->team_id == ($pick->bowl->winner ? $pick->bowl->winner->id : null) ? 'text-green-500' : '' }}">
-                                    {{ $pick->confidence }}
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                <div class="border-t border-gray-200 px-4 py-4 sm:px-6 bg-white">
-                <!-- Content goes here -->
-                    <span class="font-bold text-gray-500">Total: {{ $pointTotal }}</span>
-                </div>
-                
+                    @endforeach
+                </tbody>
+            </table>
+            <div class="border-t border-gray-200 px-4 py-4 sm:px-6 bg-white">
+            <!-- Content goes here -->
+                <span class="font-bold text-gray-500">Total: {{ $pointTotal }}</span>
             </div>
             
         </div>
+        
     </div>
+</div>
 
 
--- resources/views/livewire/score/total-score.blade.php
+++ resources/views/livewire/score/total-score.blade.php
@@ -67,11 +67,11 @@
                         <div x-show="open"
                             @click.away="open = false"
                             x-transition:enter="transition ease-out duration-300"
-                            x-transition:enter-start="opacity-0 transform scale-90"
+                            x-transition:enter-start="opacity-0 transform origin-top scale-20"
                             x-transition:enter-end="opacity-100 transform scale-100"
                             x-transition:leave="transition ease-in duration-300"
                             x-transition:leave-start="opacity-100 transform scale-100"
-                            x-transition:leave-end="opacity-0 transform scale-90"
+                            x-transition:leave-end="opacity-0 transform origin-top scale-20"
                              class="px-4 py-6" >
                                 <livewire:accordian-picks-table :userId="$score['user_id']"/>
                         </div>
