010bde0c2884a7403a171ef3b823aaca99078e4d
Christian Chapman <cdchap@protonmail.com>
2020-12-18 08:42:58 -0600

    User index clean up

--- resources/views/livewire/user/user-index.blade.php
+++ resources/views/livewire/user/user-index.blade.php
@@ -122,24 +122,23 @@
                                 <a href="#"
                                     class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
                                     <!-- Heroicon name: mail -->
-                                    <svg class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                    {{-- <svg class="w-5 h-5 text-gray-400 flex-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                                         fill="currentColor" aria-hidden="true">
                                         <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                                         <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                                    </svg>
-                                    <span class="ml-3">Email</span>
+                                    </svg> --}}
+                                    <span class="ml-3 truncate">{{$user->email}}</span>
                                 </a>
                             </div>
                             <div class="-ml-px w-0 flex-1 flex">
                                 <a href="#"
                                     class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
                                     <!-- Heroicon name: phone -->
-                                    <svg class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                        fill="currentColor" aria-hidden="true">
-                                        <path
-                                            d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                                    <svg class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                                     </svg>
-                                    <span class="ml-3">Call</span>
+                                    
+                                    <span class="ml-3">{{$user->username}}</span>
                                 </a>
                             </div>
                         </div>
