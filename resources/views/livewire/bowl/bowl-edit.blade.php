40ad6e3a4428233ef42e3c8d76c873f628cc467c
Christian Chapman <cdchap@protonmail.com>
2020-12-19 09:37:45 -0600

    finished the toggle switches

--- resources/views/livewire/bowl/bowl-edit.blade.php
+++ resources/views/livewire/bowl/bowl-edit.blade.php
@@ -141,18 +141,6 @@
                                                 :class="championship ? 'translate-x-5' : 'translate-x-0'"
                                                 class="inline-block h-5 w-5 rounded-full bg-white shadow transform transition ease-in-out duration-200"></span>
                                         </span>
-                                        <span>{{$championship}}</span>
-                                        {{-- <div class="mt-4">
-                                            <div class="flex items-center">
-                                                <input id="championship" name="championship" type="radio" wire:model="bowl.championship" value="1"
-                                                    {{ $bowl->championship ? 'checked' : '' }}
-                                                    class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out">
-                                                <label for="push_everything" class="ml-3">
-                                                    <span
-                                                        class="block text-sm leading-5 font-medium text-gray-700">championship</span>
-                                                </label>
-                                            </div>
-                                        </div> --}}
                                     </div>
                                 </div>
                             </div>
