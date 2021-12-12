04cd5fa5c4c62922ff076cdcc5223cc177b754f7
Christian Chapman <cdchap@protonmail.com>
2021-12-12 12:20:08 -0600

    Umich Stad

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -11,8 +11,8 @@
 
             <div class="hidden lg:block relative w-0 flex-1">
                 <img class="absolute inset-0 h-full w-full object-cover"
-                    src="{{ asset('images/alex-batchelor-q5IEr16VrTA-unsplash-2.jpg') }}"
-                    alt="Gaylord Family Oklahoma Memorial Stadium">
+                    src="{{ asset('images/51710934036_4a5612c2fd_k.jpg') }}"
+                    alt="Michigan Stadium">
             </div>
         </div>
     </div>
